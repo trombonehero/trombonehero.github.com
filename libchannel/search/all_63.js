3225b02d9a67613a067cdf088fbcf9afce635025
Jonathan Anderson <jonathan.anderson@ieee.org>
2012-10-17 15:25:14 +0100

    Generated Doxygen updates.

--- libchannel/search/all_63.js
+++ libchannel/search/all_63.js
@@ -2,7 +2,6 @@ var searchData=
 [
   ['channel',['channel',['../structchannel.xhtml',1,'']]],
   ['channel_5fdestroy',['channel_destroy',['../group__libchannel.xhtml#gae6b1ccc4b055de0acdbc6823365e521d',1,'channel_destroy(struct channel *c):&#160;libchannel.c'],['../group__libchannel.xhtml#gae6b1ccc4b055de0acdbc6823365e521d',1,'channel_destroy(channel *c):&#160;libchannel.c']]],
-  ['channel_5finit',['channel_init',['../group__libchannel.xhtml#ga4b73e821a54fd610cc4bf37b0df26b8f',1,'libchannel-internal.h']]],
   ['channel_5fisvalid',['channel_isvalid',['../group__libchannel.xhtml#ga39e73c5006a72b808c59705257af51dd',1,'channel_isvalid(struct channel *):&#160;libchannel.c'],['../group__libchannel.xhtml#ga39e73c5006a72b808c59705257af51dd',1,'channel_isvalid(channel *c):&#160;libchannel.c']]],
   ['channel_5fsend',['channel_send',['../group__libchannel.xhtml#ga87ef7679cb6cd0c89118ccd42b075dbe',1,'channel_send(struct channel *, struct message *):&#160;libchannel.c'],['../group__libchannel.xhtml#ga87ef7679cb6cd0c89118ccd42b075dbe',1,'channel_send(struct channel *c, struct message *m):&#160;libchannel.c']]],
   ['channel_5fsize',['channel_size',['../group__libchannel.xhtml#ga02f51b872a4fda6696d63edca2f1acab',1,'channel_size(channel *):&#160;libchannel.c'],['../group__libchannel.xhtml#ga02f51b872a4fda6696d63edca2f1acab',1,'channel_size(channel *c):&#160;libchannel.c']]],
